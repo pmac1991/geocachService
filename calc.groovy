bb1a9bc55ccf9cacb2e03071dbcd4158b2010d6a
Piotr <piomac11@gmail.com>
2015-04-16 21:10:54 +0000

    Simple calculator in groovy

--- calc.groovy
+++ calc.groovy
@@ -1,3 +1,7 @@
+/**
+ * Created by piotr_komp on 2015-04-14.
+ */
+
 import javax.swing.*
 import java.awt.*
 import java.awt.event.*
@@ -10,7 +14,8 @@ frame.contentPane.add positionLabel
 frame.contentPane.add msgLabel
 
 float currentDispValue = 0.0;
-int mode = 0; //0 bez kropki 1 kropka zostala nacisnieta
+String tempValue = '';
+boolean mode = false; //false bez kropki true kropka zostala nacisnieta
 
 def  stack = [];
 
@@ -18,22 +23,17 @@ positionLabel.setText(currentDispValue.toString());
 
 for(int i=0;i < 9; i++)
 {
-	def j = i;
-	button_$i = new JButton("$i")
-	frame.contentPane.add button_$i;
-	button_$i.addActionListener(
-	  {  
-		  if(mode == 0)
-		  {
-			  currentDispValue = currentDispValue * 10 + j;
-		  }
-		  else
-		  {
-			  currentDispValue = currentDispValue + j/10;
-		  }
-		  positionLabel.setText(currentDispValue.toString());	 
-	  } as ActionListener
-	 )
+    def j = i;
+    button_$i = new JButton("$i")
+    frame.contentPane.add button_$i;
+    button_$i.addActionListener(
+            {
+                tempValue = tempValue.concat(j.toString());
+
+                currentDispValue = Float.parseFloat(tempValue);
+                positionLabel.setText(currentDispValue.toString());
+            } as ActionListener
+    )
 };
 
 button_plus = new JButton("+")
@@ -43,6 +43,7 @@ button_div = new JButton("/")
 button_point = new JButton(".")
 button_open = new JButton("(")
 button_close = new JButton(")")
+button_equal = new JButton("=")
 
 frame.contentPane.add button_plus
 frame.contentPane.add button_minus
@@ -51,53 +52,85 @@ frame.contentPane.add button_div
 frame.contentPane.add button_open
 frame.contentPane.add button_close
 frame.contentPane.add button_point
+frame.contentPane.add button_equal
 
 button_plus.addActionListener(
- { 
-  	  currentDispValue = currentDispValue * 10 + 2;
-  	  positionLabel.setText(currentDispValue.toString());	
-  } as ActionListener
+        {
+            stack.add(currentDispValue);
+            stack.add("+");
+            currentDispValue = 0.0;
+            tempValue = '';
+            mode = false;
+            positionLabel.setText(currentDispValue.toString());
+        } as ActionListener
 )
 
 button_minus.addActionListener(
- { 
-  	  currentDispValue = currentDispValue * 10 + 2;
-  	  positionLabel.setText(currentDispValue.toString());	
-  } as ActionListener
+        {
+            stack.add(currentDispValue);
+            stack.add("-");
+            currentDispValue = 0.0;
+            tempValue = '';
+            mode = false;
+            positionLabel.setText(currentDispValue.toString());
+        } as ActionListener
 )
 
 button_mul.addActionListener(
- { 
-  	  currentDispValue = currentDispValue * 10 + 2;
-  	  positionLabel.setText(currentDispValue.toString());	
-  } as ActionListener
+        {
+            stack.add(currentDispValue);
+            stack.add("*");
+            currentDispValue = 0.0;
+            tempValue = '';
+            mode = false;
+            positionLabel.setText(currentDispValue.toString());
+        } as ActionListener
 )
 
 button_div.addActionListener(
- { 
-  	  currentDispValue = currentDispValue * 10 + 2;
-  	  positionLabel.setText(currentDispValue.toString());	
-  } as ActionListener
+        {
+            stack.add(currentDispValue);
+            stack.add("/");
+            currentDispValue = 0.0;
+            tempValue = '';
+            mode = false;
+            positionLabel.setText(currentDispValue.toString());
+        } as ActionListener
 )
 
 button_point.addActionListener(
- { 
- 	  if(mode == 0)
- 	  {
- 	  	  mode = 1;
- 	  }
- 	  if(mode == 1)
- 	  {
- 	  	  mode = 0;
- 	  }
-  	  positionLabel.setText(currentDispValue.toString());	
-  } as ActionListener
+        {
+            if(mode == false)
+            {
+                mode = true;
+                tempValue = tempValue.concat('.');
+            }
+            positionLabel.setText(currentDispValue.toString());
+        } as ActionListener
+)
+
+button_equal.addActionListener(
+        {
+            stack.add(currentDispValue);
+            String text = "";
+            for(int k=0;k < stack.size();k++)
+            {
+                    text = text + stack[k].toString();
+            }
+
+            stack.clear();
+
+            currentDispValue = 0.0;
+            tempValue = '';
+            positionLabel.setText(Eval.me(text).toString());
+
+        } as ActionListener
 )
 
 
 handleFocus = [
-  focusGained: { msgLabel.setText("Good to see you!") },
-  focusLost: { msgLabel.setText("Come back soon!") }
+        focusGained: { msgLabel.setText("Good to see you!") },
+        focusLost: { msgLabel.setText("Come back soon!") }
 ]
 
 //button_1.addFocusListener(handleFocus as FocusListener)
@@ -108,7 +141,7 @@ events = ['WindowListener', 'ComponentListener']
 handler = { msgLabel.setText("$it") }
 
 for (event in events) {
-  handleImpl = handler.asType(Class.forName("java.awt.event.${event}"))
+    handleImpl = handler.asType(Class.forName("java.awt.event.${event}"))
 }
 
 frame.show()
